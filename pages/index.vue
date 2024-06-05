c6c1c13655eff43be9c5e4151efe7e366f79f808
bazhenak <bazhena.kutergina@gmail.com>
2024-06-05 13:35:39 +0300

    init project

---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-col>
+    <v-container>
+      <v-col align="center">
+
+        <h3 style="font-weight: 400;">Figma connects everyone in the design process so teams can deliver better products
+          faster.</h3>
+        <br>
+        <v-btn outlined color="black" @click="scrollToForm">
+          Sign up free
+        </v-btn>
+      </v-col>
+    </v-container>
+    <v-img
+      src="https://cdn.sanity.io/images/599r6htc/localized/455e29e0d05e64300b4a2aa99bcd25fa10a341a7-6969x2010.png?rect=0,0,6969,2008&w=2016&h=581&q=75&fit=max&auto=format"
+      style="width: 150%;" />
+    <v-container>
+      <SocialCard />
+    </v-container>
+    <LastDivider />
+    <v-container ref="registerForm">
+      <RegisterForm />
+    </v-container>
+  </v-col>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+  methods: {
+    scrollToForm() {
+      const el = this.$refs.registerForm;
+      if (el) {
+        el.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }
+}
+</script>
